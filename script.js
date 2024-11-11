4dea14c13fcd1f955c28ff1ed34cdc9d08831821
Benedictekoder <benedictekoder@gmail.com>
2024-11-11 10:55:56 +0100

    added java

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello!');
+function person() {
+    console.log ("hallo")
+
+    const userName ="B";
+    const userAge = 18;
+    let isOnline = true;
+    
+    //userName="Julius";
+    //userAge =29;
+    isOnline= false;
+    
+    console.log(userName,userAge,isOnline);
+}
+
+person();
+
+function simpleCalculator(num1,num2) {
+const sum = num1 + num2;
+
+console.log(sum);
+}
+
+simpleCalculator(12,2);
+
+
